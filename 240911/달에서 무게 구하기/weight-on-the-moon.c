6b3e57f0553e14b6b32b537533e0277dba715a41
imsanoh <72605022+imsanoh@users.noreply.github.com>
2024-09-11 01:30:47 +0900

    Title: 달에서 무게 구하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240911/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    float b;
+    float c;
+
+    a = 13;
+    b = 0.165;
+    c = a*b;
+
+    printf("%d * %f = %f", a, b, c);
+
+    return 0;
+}
