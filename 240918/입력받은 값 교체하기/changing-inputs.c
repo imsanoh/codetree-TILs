7dee01eed054035f542058e86475f54f73851c55
imsanoh <72605022+imsanoh@users.noreply.github.com>
2024-09-18 21:10:26 +0900

    Title: 입력받은 값 교체하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240918/입력받은 값 교체하기/changing-inputs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    int b;
+    int c;
+    
+    
+    scanf("%d %d", &a, &b);
+
+    c = a;
+    a = b;
+    b = c;
+
+    printf("%d %d", a, b);
+
+    return 0;
+}
