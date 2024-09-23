535691077e68921a42526de3f248ee5fdc740b38
imsanoh <72605022+imsanoh@users.noreply.github.com>
2024-09-23 12:41:30 +0900

    Title: 입력받은 값과 합 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240923/입력받은 값과 합 출력/output-of-value-and-sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    int b;
+    int c;
+    
+    scanf("%d %d", &a, &b);
+
+        c = a+b;
+
+    printf("%d %d %d", a, b, c);
+
+    return 0;
+}
