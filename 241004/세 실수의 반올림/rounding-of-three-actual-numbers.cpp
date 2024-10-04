7463cac77e4e02e1c876ca9223dea27a8a2544aa
imsanoh <72605022+imsanoh@users.noreply.github.com>
2024-10-04 15:39:53 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241004/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    float a, b, c;
+
+    scanf("%f\n%f\n%f", &a, &b, &c);
+
+    printf("%.3lf\n%.3lf\n%.3lf", a, b, c);
+
+    return 0;
+}
